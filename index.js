380183ed2a19720cdf4b9632b7ad64a6cfbf850e
Alonso0213 <alonsocupido51@gmail.com>
2023-07-31 15:52:28 +0200

    update

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+fs.writeFile('./message.txt','Joel',(err,data)=>{
+    if (err)
+    console.log('sorry');
+   console.log(data);
+  })
 fs.readFile('./message.txt', 'utf-8', (err, data) => {
     if (err)
     console.log('sorry');
